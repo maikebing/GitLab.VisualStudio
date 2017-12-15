8da1c3a9344f9ca05d06308f9c2979d7a6508e18
MysticBoy <mysticboy@live.com>
2017-12-15 20:10:19 +0800

    /p:DeployExtension=false

--- rebuildself.cmd
+++ rebuildself.cmd
@@ -2,4 +2,4 @@
 setlocal EnableDelayedExpansion
 call  "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\Common7\Tools\VsDevCmd.bat"
 chcp 65001
-msbuild.exe   %~dp0\%1   /t:Rebuild   /p:Configuration=Release;Platform="Any CPU"
+msbuild.exe   %~dp0\%1   /t:Rebuild   /p:Configuration=Release;Platform="Any CPU" /p:DeployExtension=false
