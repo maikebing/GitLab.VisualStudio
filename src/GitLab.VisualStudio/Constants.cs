054c8c5612e176bf0eec9abf36adc327dcf4b580
MysticBoy <mysticboy@live.com>
2017-12-20 13:26:03 +0800

    Update Constants.cs

--- src/GitLab.VisualStudio/Constants.cs
+++ src/GitLab.VisualStudio/Constants.cs
@@ -11,7 +11,7 @@ namespace GitLab.VisualStudio
         public const string guidGitLabPkgString = "54803a44-49e0-4935-bba4-7d7d91682273";
         public const string guidOpenOnGitLabPkgString = "F43CFE82-0372-4230-9162-038E51408B2F";
         public const string guidOpenOnGitLabCmdSetString = "72B54F2E-FE93-4950-88BF-C536D1CFD91F";
-
+        public const string Version = "1.0";
         public static readonly Guid guidOpenOnGitLabCmdSet = new Guid(guidOpenOnGitLabCmdSetString);
     };
 
