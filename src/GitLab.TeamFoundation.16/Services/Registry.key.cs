0dcee30a5a70f09b2f447d328b133a01ee3b48dc
MysticBoy <mysticboy@live.com>
2018-12-07 15:53:29 +0800

    Visual Studio 2019 support

---
+++ src/GitLab.TeamFoundation.16/Services/Registry.key.cs
@@ -0,0 +1,7 @@
+﻿namespace GitLab.TeamFoundation.Services
+{
+    partial class Registry
+    {
+        private const string TEGitKey = @"Software\Microsoft\VisualStudio\15.0\TeamFoundation\GitSourceControl";
+    }
+}
