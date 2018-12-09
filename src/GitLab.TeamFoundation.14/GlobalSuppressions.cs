274e5207523c83fef04e2629a20eb5fb8b16614b
MysticBoy <mysticboy@live.com>
2018-12-09 17:13:38 +0800

    update suppressions

---
+++ src/GitLab.TeamFoundation.14/GlobalSuppressions.cs
@@ -0,0 +1,19 @@
+﻿
+// This file is used by Code Analysis to maintain SuppressMessage 
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given 
+// a specific target and scoped to a namespace, type, member, etc.
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0044:Add readonly modifier", Justification = "<Pending>", Scope = "member", Target = "~F:GitLab.TeamFoundation.Home.GitLabNavigationItem._project")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0044:Add readonly modifier", Justification = "<Pending>", Scope = "member", Target = "~F:GitLab.TeamFoundation.Home.GitLabNavigationItem._branch")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0044:Add readonly modifier", Justification = "<Pending>", Scope = "member", Target = "~F:GitLab.TeamFoundation.TeamExplorerServices._web")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0044:Add readonly modifier", Justification = "<Pending>", Scope = "member", Target = "~F:GitLab.TeamFoundation.TeamExplorerServices._git")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Code Quality", "IDE0051:Remove unused private members", Justification = "<Pending>", Scope = "member", Target = "~F:GitLab.TeamFoundation.Home.GitLabNavigationItem._project")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Code Quality", "IDE0051:Remove unused private members", Justification = "<Pending>", Scope = "member", Target = "~F:GitLab.TeamFoundation.Home.GitLabNavigationItem._branch")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Code Quality", "IDE0051:Remove unused private members", Justification = "<Pending>", Scope = "member", Target = "~F:GitLab.TeamFoundation.Home.TeamExplorerServices._git")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Code Quality", "IDE0051:Remove unused private members", Justification = "<Pending>", Scope = "member", Target = "~F:GitLab.TeamFoundation.Home.TeamExplorerServices._web")]
+
+
+
+
+
