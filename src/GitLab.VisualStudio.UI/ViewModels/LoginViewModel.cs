007041fb32890e488b9aa818927614e7b0b52c67
MysticBoy <mysticboy@live.com>
2022-05-04 23:58:14 +0800

    测试 网页登录方式

--- src/GitLab.VisualStudio.UI/ViewModels/LoginViewModel.cs
+++ src/GitLab.VisualStudio.UI/ViewModels/LoginViewModel.cs
@@ -1,11 +1,16 @@
-﻿using GitLab.VisualStudio.Shared;
+﻿using EmbedIO;
+using EmbedIO.WebApi;
+using GitLab.VisualStudio.Shared;
 using GitLab.VisualStudio.Shared.Helpers;
 using GitLab.VisualStudio.Shared.Helpers.Commands;
+using GitLab.VisualStudio.UI.Properties;
 using Microsoft.VisualStudio.Threading;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.IO;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
@@ -26,7 +31,10 @@ namespace GitLab.VisualStudio.UI.ViewModels
         private readonly IShellService _shell;
         private readonly IStorage _storage;
         private readonly IWebService _web;
-
+        private readonly WebServer _webServer;
+        private readonly string REDIRECT_URI = "http://localhost:65056/";
+        private readonly string APPID = GitApp.client_id;
+        private readonly string STATE = "";
         public LoginViewModel(IDialog dialog, IPasswordMediator mediator, IMessenger messenger, IShellService shell, IStorage storage, IWebService web)
         {
             _dialog = dialog;
@@ -48,8 +56,63 @@ namespace GitLab.VisualStudio.UI.ViewModels
             _forgetPasswordCommand = new DelegateCommand(OnForgetPassword);
             _activeAccountCommand = new DelegateCommand(OnActiveAccount);
             _signupCommand = new DelegateCommand(OnSignup);
+            STATE = radomstrsbyling(chars, 8);
+            _webServer = CreateWebServer(REDIRECT_URI);
+            _webServer.RunAsync();
+        }
+        Random random = new Random();
+        private string chars = "ABCDEFGHIJKLMNOPQRSTUWVXYZ0123456789abcdefghijklmnopqrstuvwxyz";
+        private string radomstrsbyling(string chars, int length)
+        {
+            return new string(Enumerable.Repeat(chars, length).Select(s => s[random.Next(chars.Length)]).ToArray());
         }
 
+        // Create and configure our web server.
+        private WebServer CreateWebServer(string url)
+        {
+            var server = new WebServer(o => o
+                    .WithUrlPrefix(url)
+                    .WithMode(HttpListenerMode.EmbedIO))
+                // First, we will configure our web server by adding Modules.
+                .WithLocalSessionManager()
+               .OnGet("/", h =>
+               {
+                   var query = h.GetRequestQueryData();
+
+                   var code = query["code"];
+                   var state = query["state"];
+                   if (STATE == state)
+                   {
+
+
+                       var parameters = $"client_id={APPID}&code={code}&grant_type=authorization_code&redirect_uri={REDIRECT_URI}&code_verifier=CODE_VERIFIER";
+                       //'https://gitlab.example.com/oauth/token', parameters
+
+                   }
+                   else
+                   {
+                       h.SendStandardHtmlAsync(500, c => c.WriteLine($"state error"));
+                   }
+                   return Task.CompletedTask;
+               });
+            return server;
+        }
+        public Task SendHtmlAsync(IHttpContext @this, string title, string body)
+        {
+            if (!HttpStatusDescription.TryGet(200, out string description))
+            {
+                throw new ArgumentException("Status code has no standard description.", "statusCode");
+            }
+            @this.Response.StatusCode = 200;
+            @this.Response.StatusDescription = description;
+            @this.Response.ContentType = "text/html";
+            @this.Response.ContentEncoding = Encoding.UTF8;
+            using (TextWriter textWriter = @this.OpenResponseText(Encoding.UTF8))
+            {
+                textWriter.Write("<html><head><meta charset=\"{2}\"><title>{0}</title></head><body>{1}</body></html>", title, body, description, Encoding.UTF8.WebName);
+            }
+            return Task.CompletedTask;
+        }
         private string _host;
 
         [Required(ErrorMessageResourceType = typeof(Strings), AllowEmptyStrings = false, ErrorMessageResourceName = "Login_HostIsRequired")]
@@ -181,6 +244,13 @@ namespace GitLab.VisualStudio.UI.ViewModels
 
         private void OnLogin()
         {
+            string openurl = $"https://gitlab.com/oauth/authorize?client_id=60e86660b759079b66f429f506166fe1cd8cd5fd350476b06801ea973e46ac93&redirect_uri={REDIRECT_URI}&response_type=code&state=STATE&scope=api&code_challenge=CODE_CHALLENGE&code_challenge_method=S256";
+            var browser = new System.Diagnostics.Process()
+            {
+                StartInfo = new System.Diagnostics.ProcessStartInfo(openurl) { UseShellExecute = true }
+            };
+            browser.Start();
+
             Validate();
             if (HasErrors)
             {
